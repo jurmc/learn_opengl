06b51c3bc96e79e59e3b7286816e430dcb0f2c32
Michal Jurek <jurmcc@gmail.com>
2025-06-28 19:23:45 +0200

    Hello Two Trinagles: done

--- main.cpp
+++ main.cpp
@@ -53,10 +53,15 @@ int main(void) {
     glViewport(0, 0, 800, 600);
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f,
          0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+        -0.5f, -0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f
         };
+    unsigned int indices[] = {
+        0, 1, 2, // first triangle
+        3, 0, 2, // second triangle
+    };
 
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
@@ -66,6 +71,12 @@ int main(void) {
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
@@ -109,6 +120,8 @@ int main(void) {
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
 
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
     while (!glfwWindowShouldClose(window)) {
         // input
         processInput(window);
@@ -116,7 +129,7 @@ int main(void) {
         // render commands
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // hanlde events, and swap buffers
         glfwSwapBuffers(window);
