8988f665cbff0a4eec4d2dae4636141dab782c1c
Jurek Michal-AMJ018 <Michal.Jurek@motorolasolutions.com>
2025-07-05 23:23:02 +0200

    Shaders with uniforms

--- excercises/1.getting_started/src/triangle2-shaders-uniforms.cpp
+++ excercises/1.getting_started/src/triangle2-shaders-uniforms.cpp
@@ -1,6 +1,8 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+#include <math.h>
+
 #include<iostream>
 
 const char *vsSource = "                                  \n"
@@ -14,17 +16,10 @@ const char *vsSource = "                                  \n"
 const char *fsSource1 = "\n"
     "#version 330 core                             \n"
     "out vec4 FragColor;                           \n"
+    "uniform vec4 vertexColorUni;                  \n"
     "                                              \n"
     "void main() {                                 \n"
-    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); \n"
-    "}                                             \n";
-
-const char *fsSource2 = "\n"
-    "#version 330 core                             \n"
-    "out vec4 FragColor;                           \n"
-    "                                              \n"
-    "void main() {                                 \n"
-    "    FragColor = vec4(1.0f, 1.0f, 0.2f, 1.0f); \n"
+    "    FragColor = vertexColorUni;               \n"
     "}                                             \n";
 
 void processInput(GLFWwindow *window) {
@@ -64,45 +59,18 @@ int main(void) {
     std::cout << "Max vertex attributes: " << nAttributes << std::endl;
 
     float vertices1[] = {
-        0.5f,  0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f};
+        0.0f,  0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f};
 
-    float vertices2[] = {
-        0.45f,  0.5f, 0.0f,
-        -0.55f, 0.5f, 0.0f,
-        -0.55f, -0.5f, 0.0f};
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
 
-    unsigned int indices[] = {
-        0, 1, 2, // first triangle
-        3, 0, 2, // second triangle
-    };
-
-    unsigned int VAO1;
-    glGenVertexArrays(1, &VAO1);
-    glBindVertexArray(VAO1);
-
-    unsigned int VBO1;
-    glGenBuffers(1, &VBO1);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
-
-//    unsigned int EBO;
-//    glGenBuffers(1, &EBO);
-//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-//    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
-//
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    unsigned int VAO2;
-    glGenVertexArrays(1, &VAO2);
-    glBindVertexArray(VAO2);
-
-    unsigned int VBO2;
-    glGenBuffers(1, &VBO2);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -130,41 +98,19 @@ int main(void) {
         std::cout << infoLog << std::endl;
     }
 
-    unsigned int fs2 = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fs2, 1, &fsSource2, NULL);
-    glCompileShader(fs2);
-    glGetShaderiv(vs, GL_COMPILE_STATUS, &rc);
-    if (!rc) {
-        glGetShaderInfoLog(fs2, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED" << std::endl;
-        std::cout << infoLog << std::endl;
-    }
-
-    unsigned int shaderProgram1 = glCreateProgram();
-    glAttachShader(shaderProgram1, vs);
-    glAttachShader(shaderProgram1, fs1);
-    glLinkProgram(shaderProgram1);
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vs);
+    glAttachShader(shaderProgram, fs1);
+    glLinkProgram(shaderProgram);
     glGetProgramiv(vs, GL_LINK_STATUS, &rc);
     if (!rc) {
-        glGetProgramInfoLog(shaderProgram1, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED" << std::endl;
-        std::cout << infoLog << std::endl;
-    }
-
-    unsigned int shaderProgram2 = glCreateProgram();
-    glAttachShader(shaderProgram2, vs);
-    glAttachShader(shaderProgram2, fs2);
-    glLinkProgram(shaderProgram2);
-    glGetProgramiv(vs, GL_LINK_STATUS, &rc);
-    if (!rc) {
-        glGetProgramInfoLog(shaderProgram2, 512, NULL, infoLog);
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED" << std::endl;
         std::cout << infoLog << std::endl;
     }
 
     glDeleteShader(vs);
     glDeleteShader(fs1);
-    glDeleteShader(fs2);
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
@@ -176,19 +122,19 @@ int main(void) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // 1st triangle
-        glUseProgram(shaderProgram1);
-        glBindVertexArray(VAO1);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glUseProgram(shaderProgram);
 
-        // 2nd triangle
-        glUseProgram(shaderProgram2);
-        glBindVertexArray(VAO2);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // varying green
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "vertexColorUni");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
-        // hanlde events, and swap buffers
+        // draw triangle
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // handle events, and swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
