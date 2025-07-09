2610a837860451725d35e586b6aab559c42afdec
Michal Jurek <jurmcc@gmail.com>
2025-07-09 23:46:12 +0200

    Textures: work started

--- excercises/1.getting_started/src/triangle1.cpp
+++ excercises/1.getting_started/src/triangle1.cpp
@@ -87,11 +87,6 @@ int main(void) {
     glBindBuffer(GL_ARRAY_BUFFER, VBO1);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
 
-//    unsigned int EBO;
-//    glGenBuffers(1, &EBO);
-//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-//    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
-//
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
