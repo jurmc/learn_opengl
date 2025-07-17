0662a26e48f9af16ece595c57642b7b9c39424c7
Michal Jurek <jurmcc@gmail.com>
2025-07-17 22:55:29 +0200

    Going 3D: ongoing

--- excercises/1.getting_started/src/textures3-more3d.cpp
+++ excercises/1.getting_started/src/textures3-more3d.cpp
@@ -71,8 +71,6 @@ int main(void) {
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
 
