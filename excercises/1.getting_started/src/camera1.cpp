e0cbc19e941243ac368f27686e52815a4fecb1ff
Michal Jurek <jurmcc@gmail.com>
2025-07-27 21:21:23 +0200

    wip: camera

--- excercises/1.getting_started/src/camera1.cpp
+++ excercises/1.getting_started/src/camera1.cpp
@@ -11,9 +11,8 @@
 #include <stb_image.h>
 
 #include <iostream>
-#include <list>
 
-void processInput(GLFWwindow *window, float *fov_angle,
+void processInput(GLFWwindow *window, float *fov_angle, float *aspect_ratio,
         float *cam_x, float *cam_z) {
     float fov_angle_incr = 0.05;
     float aspect_ratio_incr = 0.05;
@@ -28,9 +27,9 @@ void processInput(GLFWwindow *window, float *fov_angle,
     }
 
     if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
-        ;
+        *aspect_ratio = *aspect_ratio - aspect_ratio_incr;
     } else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-        ;
+        *aspect_ratio = *aspect_ratio + aspect_ratio_incr;
     }
 
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
@@ -135,7 +134,7 @@ int main(void) {
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    Shader shader("shaders/texture3-minecraft-cubes.vs", "shaders/texture3-minecraft-cubes.fs");
+    Shader shader("shaders/texture3-more3d.vs", "shaders/texture3-more3d.fs");
 
     int width, height, nChannels;
     // Image and textture
@@ -148,34 +147,57 @@ int main(void) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     stbi_set_flip_vertically_on_load(true);
-    //unsigned char *data = stbi_load("/home/jurmc/game_assets/SBS - Sandbox Style Terrain - Texture Pack/Textures/Dirt/cubeDirt_1.png", &width, &height, &nChannels, 0);
-    unsigned char *data = stbi_load("/home/jurmc/game_assets/SBS - Sandbox Style Terrain - Texture Pack/Textures/Grass/cubeGreen_1.png", &width, &height, &nChannels, 0);
+    unsigned char *data = stbi_load("textures/container.jpg", &width, &height, &nChannels, 0);
     if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     } else {
         std::cout << "Cannot load image" << std::endl;
     }
     stbi_image_free(data);
 
+    unsigned int texture2;
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    unsigned char *data2 = stbi_load("textures/awesomeface.png", &width, &height, &nChannels, 0);
+    if (data2) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data2);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Cannot load image" << std::endl;
+    }
+    stbi_image_free(data2);
+
     shader.use();
     shader.setInt("texture1", 0);
+    shader.setInt("texture2", 1);
 
-    std::list<glm::vec3> cubePositions;
-    for (float x = 0; x < 10; ++x) {
-        for (float z = 0; z > -10; --z) {
-            cubePositions.push_back(glm::vec3(2*x, abs(z)/2.0f, 2*z));
-        }
-    }
+    glm::mat4 projection = glm::mat4(1.0f);
+    glm::vec3 cubePositions[] = {
+        glm::vec3(  0.0f,   0.0f,   0.0f),
+        glm::vec3(  2.0f,   5.0f, -15.0f),
+        glm::vec3( -1.5f,  -2.2f,  -2.5f),
+        glm::vec3( -3.8f,  -2.0f, -12.3f),
+        glm::vec3(  2.4f,  -0.4f,  -3.5f),
+        glm::vec3( -1.7f,   3.0f,  -7.5f),
+        glm::vec3(  1.3f,  -2.0f,  -2.5f),
+        glm::vec3(  1.5f,   2.0f,  -2.5f),
+        glm::vec3(  1.5f,   0.2f,  -1.5f),
+        glm::vec3( -1.3f,   1.0f,  -1.5f)
+    };
 
     float fov_angle = 45.0f;
     float aspect_ratio = 800.0f / 600.0f;
-    float cam_x  = -5.0f;
-    float cam_z = -10.0f;
+    float cam_x  = 0.0f;
+    float cam_z = -3.0f;
 
     while (!glfwWindowShouldClose(window)) {
         // input
-        processInput(window, &fov_angle,
+        processInput(window, &fov_angle, &aspect_ratio,
                 &cam_x, &cam_z);
 
         // render commands
@@ -184,28 +206,41 @@ int main(void) {
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
 
         shader.use();
 
-        glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(cam_x, -1.0f, cam_z));
-        int viewLoc = glGetUniformLocation(shader.ID, "view");
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-
-        glm::mat4 projection = glm::mat4(1.0f);
-
-        for (glm::vec3 cube : cubePositions)
+        for (unsigned int i = 1; i < 10; ++i)
         {
+            // camera
+            const float r = 10.0f;
+            float cam_x = sin(glfwGetTime()) * r;
+            float cam_z = cos(glfwGetTime()) * r;
+            glm::vec3 cameraPos = glm::vec3(cam_x, 0.0f, cam_z);
+            glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+            glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+            //glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+            glm::mat4 newView = glm::lookAt(cameraPos, cameraTarget, cameraUp);
+            int viewLoc = glGetUniformLocation(shader.ID, "view");
+            glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(newView));
+
             projection = glm::perspective(glm::radians(fov_angle), aspect_ratio, 0.1f, 100.0f);
             int projectionLoc = glGetUniformLocation(shader.ID, "projection");
             glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
             glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cube);
+            model = glm::translate(model, cubePositions[i]);
 
-            model = glm::rotate(model, glm::radians(20.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-            int modelLoc = glGetUniformLocation(shader.ID, "model");
-            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+            if ( i % 3 == 0) {
+                model = glm::rotate(model, glm::radians(20.0f + (float)glfwGetTime() * i), glm::vec3(0.5f, 1.0f, 0.0f));
+                int modelLoc = glGetUniformLocation(shader.ID, "model");
+                glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+            } else {
+                model = glm::rotate(model, glm::radians(20.0f * i), glm::vec3(0.5f, 1.0f, 0.0f));
+                int modelLoc = glGetUniformLocation(shader.ID, "model");
+                glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+            }
 
             glBindVertexArray(VAO);
             glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / 5);
